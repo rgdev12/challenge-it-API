87615b56fd734dccda8c07a024bdc1ef868e7f6f
Kevin Carrillo <acarrillo@mibluemedical.com>
2024-12-11 19:26:55 -0600

    funcino para preview e instalacion laravel 8

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/filter', [\App\Http\Controllers\ApiController::class, 'filter'])->name('filter');
+Route::post('/filter', [App\Http\Controllers\ApiController::class, 'filter']);
